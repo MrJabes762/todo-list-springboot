d10881b130837fd7f2e8aab5f4256b8380dd8f7b
Mr Jabes <jabessilva31@gmail.com>
2024-09-09 02:24:06 -0300

    Atualizações; Criação de uma entidade Todo

---
+++ src/main/java/br/com/mrjabes/todo_list_springboot/model/Todo.java
@@ -0,0 +1,10 @@
+package br.com.mrjabes.todo_list_springboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Todos")
+public class Todo {
+    
+}
