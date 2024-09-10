604cd1e0f37d1258bc292df3b0307889835e5ca7
Mr Jabes <jabessilva31@gmail.com>
2024-09-10 01:04:14 -0300

    Atualizações:
    *Finalizando o Primeiro teste Automatizado Unitário

--- src/test/java/br/com/mrjabes/todo_list_springboot/TodoListSpringbootApplicationTests.java
+++ src/test/java/br/com/mrjabes/todo_list_springboot/TodoListSpringbootApplicationTests.java
@@ -3,12 +3,13 @@ package br.com.mrjabes.todo_list_springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import br.com.mrjabes.todo_list_springboot.model.todo.Prioridade;
 import br.com.mrjabes.todo_list_springboot.model.todo.Todo;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TodoListSpringbootApplicationTests {// testes automatizados 
 
 	@Autowired// chamar o endpoints
