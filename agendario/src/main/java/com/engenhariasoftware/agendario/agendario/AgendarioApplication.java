ec1c3a937cf2aea78a5a1cc7c62296998dc3093a
Mr Jabes <jabessilva31@gmail.com>
2025-02-10 00:50:59 -0300

    Atualizações: Novas Configuracoes

---
+++ agendario/src/main/java/com/engenhariasoftware/agendario/agendario/AgendarioApplication.java
@@ -0,0 +1,13 @@
+package com.engenhariasoftware.agendario.agendario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendarioApplication.class, args);
+	}
+
+}
---
+++ agendario/src/test/java/com/engenhariasoftware/agendario/agendario/AgendarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engenhariasoftware.agendario.agendario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
