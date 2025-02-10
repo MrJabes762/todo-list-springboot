ee80c3b6425a0828e8b4cd8fdbe80ffeeb319f01
Mr Jabes <jabessilva31@gmail.com>
2025-02-10 00:29:42 -0300

    Atualizações: Reajuste de Projeto

--- src/main/java/br/com/mrjabes/todo_list_springboot/data/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.mrjabes.todo_list_springboot.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.mrjabes.todo_list_springboot.model.todo.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo,Long>{
-    
-}
